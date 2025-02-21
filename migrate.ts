0ddb07453ce9ad27abc3518c4bcee450e15761cc
Malik Soltukiev <malSolt99@gmail.com>
2025-02-21 11:27:58 +0100

    deleted migrate.ts file

--- migrate.ts
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import { migrate } from "drizzle-orm/postgres-js/migrator";
-import { db, connection } from "./db";
-
-(async () => {
-  await migrate(db, { migrationsFolder: "./drizzle" });
-  await connection.end();
-})();
